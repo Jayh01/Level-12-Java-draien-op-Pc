e776c519063bec7a63ad83a4f2177020a271e781
Jayh01 <84378798+Jayh01@users.noreply.github.com>
2021-08-04 13:41:53 +0200

    Add files via upload

---
+++ Level 12 - Java draien op Pc/script.js
@@ -0,0 +1,12 @@
+console.log("Hallo Winc Academy studenten")
+
+// Dit is een grote some
+
+/*let some =  1230941 * 1823794;
+console.log ('uitkomst some-1=', some);
+
+let some2 = 637263 / 54 ;
+console.log ('uitkomst some-2 =',some2);*/
+
+// vraag 3: je krijgt niks tezien wan tdit is een comment
+// vraag 6: nee je ziet geen uitkomst
